5ee41f083e399332e9017c849654aa0345c04141
jakeribu <95576607+jacy913@users.noreply.github.com>
2024-04-10 19:37:40 +0800

    fixed: routes.js, db.js, handlebars, and possibly mongodb connection

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
-const bcrypt = require('bcrypt');
 const path = require('path');
+const { connectToDB } = require('./server/models/db.js');
+
+const routes = require('./server/routes/routes.js');
+
+app.use('/', routes);
 
 // Configure Handlebars as the template engine
 app.engine('hbs', exphbs.engine({
-  defaultLayout: 'main',
+  defaultLayout: 'default',
   extname: 'hbs',
   layoutsDir: __dirname + '/views/layouts', 
 }));
@@ -18,86 +22,15 @@ app.use(express.static('public'));
 
 // Define the main layout
 app.layouts = {
-  main: __dirname + '/views/layouts/main.hbs'
+  main: path.join(__dirname, 'views/layouts/default.hbs')
 };
 
-// Serve the homepage template at /
-app.get('/', (req, res) => {
-  res.render('home', {
-    layout: 'home',
-    title: 'Threadle',
-    css: 'main.css'
-  });
-});
+// set the folder `public` as folder containing static assets
+// such as css, js, and image files
+app.use(express.static('public'));
 
-app.get('/main', (req, res) => {
-  res.render('main', {
-    layout: 'default',
-    title: 'Threadle • About',
-    css: 'main.css'
-  });
-})
-
-app.get('/about', (req, res) => {
-  res.render('about', {
-    layout: 'default',
-    title: 'Threadle • About',
-    css: 'main.css'
-  });
-})
-
-app.get('/homepage', (req, res) => {
-  res.render('homepage', {
-    layout: 'default',
-    title: 'Threadle • Home',
-    css: 'main.css'
-  });
-})
-
-app.get('/user_login', (req, res) => {
-  res.render('user_login', {
-    layout: 'default',
-    title: 'Threadle • Login',
-    css: 'user_login_signup.css'
-  });
-});
-
-app.get('/user_signup', (req, res) => {
-  res.render('user_signup', {
-    layout: 'default',
-    title: 'Threadle • Sign up',
-    css: 'user_login_signup.css'
-  });
-});
-
-app.get('/profile_user', (req, res) => {
-  res.render('profile_user', {
-    layout: 'user-profile',
-    title: 'Threadle • User Profile',
-    css: 'main.css'
-  });
-});
-
-app.post('/user_login_signup', async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      username: req.body.username,
-      password: hashedPassword,
-    });
-    await newUser.save();
-
-    console.log('New user created:', newUser);
-
-    res.redirect('/user-profile');
-
-  } catch {
-    res.redirect('/user_login_signup');
-  }
-});
+// define the paths contained in `./routes/routes.js`
+app.use('/', routes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
@@ -105,3 +38,5 @@ app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
   console.log(`Listening at port ${PORT}`);
 });
+
+connectToDB();
