d2fc134e01f2df6fed5b761133eda5b0130da35b
kaliusolence <luisa_jacob@dlsu.edu.ph>
2024-04-12 10:20:12 +0800

    updated user profile, user, and main

--- public/common/user-profile.js
+++ public/common/user-profile.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
     $('#edit-profile-btn').click(function() {
+        console.log("Edit button clicked");
         $(".profile-panel").hide();
         $(".edit-profile-panel").show();
     });
 
     $('#editForm').on('submit', function(event) {
         event.preventDefault();
+        console.log("Edit button clicked");
         $(".edit-profile-panel").hide();
         $(".profile-panel").show();
     });
